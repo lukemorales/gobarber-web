30569996a2f797fb6a8c674daa2ab61e3eefb76c
Luke Morales <lukemorales@live.com>
2019-07-28 17:39:56 -0300

    chore: Added Unform

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '@/assets/img/gobarber_logo.svg';
 
 export default function LogIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <h1>
         <img src={logo} alt="GoBarber" />
       </h1>
 
-      <form>
-        <input type="email" placeholder="Your e-mail" />
-        <input type="password" placeholder="Your password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Your password" />
         <button type="submit">Log In</button>
         <Link to="/signup">Sign Up</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '@/assets/img/gobarber_logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <h1>
         <img src={logo} alt="GoBarber" />
       </h1>
 
-      <form>
-        <input placeholder="Your name" />
-        <input type="email" placeholder="Your e-mail" />
-        <input type="password" placeholder="Your password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Your name" />
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Your password" />
         <button type="submit">Create Account</button>
         <Link to="/">I already have an account</Link>
-      </form>
+      </Form>
     </>
   );
 }
