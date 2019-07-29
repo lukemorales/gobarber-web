05665c7c93f0a09e336055b1557db06bca3351b9
Luke Morales <lukemorales@live.com>
2019-07-28 22:34:25 -0300

    interface: Header Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './Header_Styles';
+
+import logo from '@/assets/img/gobarber_logo_purple.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <h1>
+            <img src={logo} alt="GoBarber" />
+          </h1>
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Luke Morales</strong>
+              <Link to="/profile">My Profile</Link>
+            </div>
+            <img src="https://ui-avatars.com/api/?background=7159c1&color=fff&name=Luke+Morales" alt="Luke Morales" />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
