f8c77abd6b89bf7cefb800e7bd0c1f365c81cd65
Luke Morales <lukemorales@live.com>
2019-07-28 21:21:57 -0300

    feat: Display Feedback to User with Toastify

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '@/services/api';
 import { logInSuccess, signFailure } from './actions';
@@ -16,7 +17,7 @@ export function* logIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('User is not a provider');
+      toast.error('User is not a provider.');
       return;
     }
 
@@ -24,6 +25,7 @@ export function* logIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Authentication failure. Please verify your data.');
     yield put(signFailure());
   }
 }
