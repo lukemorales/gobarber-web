c848d4a275ee5449f62b71dc1950580d3fd09a4d
Luke Morales <lukemorales@live.com>
2019-07-29 23:34:02 -0300

    interface: Profile Page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,13 +1,28 @@
 import React from 'react';
-// import {} from './Profile_Styles';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
+
+import { Container } from './Profile_Styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
-    <>
-      <h1>Profile Component</h1>
-    </>
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="Your E-mail" />
+        <hr />
+        <Input name="oldPassword" placeholder="Old Password" />
+        <Input name="password" placeholder="New Password" />
+        <Input name="Confirmpassword" placeholder="Confirm New Password" />
+
+        <button type="submit">Update Profile</button>
+      </Form>
+
+      <button type="button">Log Out</button>
+    </Container>
   );
-};
-
-
-
+}
--- src/pages/Profile/Profile_Styles.js
+++ src/pages/Profile/Profile_Styles.js
@@ -1,6 +1,80 @@
 import styled from 'styled-components';
+import { lighten, darken } from 'polished';
 
-export const $ = styled.`
-	
+export const Container = styled.section`
+  max-width: 600px;
+  margin: 50px auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.15);
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: #bbb;
+      }
+    }
+
+    span {
+      background: #f64c75;
+      color: #6d2335;
+      padding: 4px 8px;
+      margin: -12px 0 10px;
+      border-radius: 0 0 4px 4px;
+      font-weight: bold;
+    }
+
+    hr {
+      height: 1px;
+      background: rgba(255, 255, 255, 0.2);
+      margin: 10px 0 20px;
+    }
+
+    button {
+      background: #3b9eff;
+      margin: 5px 0 0;
+      padding: 12px;
+      height: 44px;
+      font-weight: bold;
+      border-radius: 4px;
+      color: #15385d;
+      transition: background 150ms ease-in-out;
+
+      &:hover {
+        background: ${lighten(0.03, '#3b9eff')};
+      }
+
+      &:active {
+        background: ${darken(0.05, '#3b9eff')};
+      }
+    }
+  }
+
+  > button {
+    width: 100%;
+    background: #f64c75;
+    margin: 15px 0 0;
+    padding: 12px;
+    height: 44px;
+    font-weight: bold;
+    border-radius: 4px;
+    color: #5d313b;
+    transition: background 150ms ease-in-out;
+
+    &:hover {
+      background: ${lighten(0.02, '#f64c75')};
+    }
+
+    &:active {
+      background: ${darken(0.05, '#f64c75')};
+    }
+  }
 `;
-
