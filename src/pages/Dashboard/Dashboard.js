3723a4267666b4c70768268a9f9e906a1c6a65cd
Luke Morales <lukemorales@live.com>
2019-07-30 01:01:39 -0300

    feat: Navigation Between Dates

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import { enUS } from 'date-fns/locale';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 import api from '@/services/api';
@@ -6,15 +8,27 @@ import api from '@/services/api';
 import { Container, Time } from './Dashboard_Styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(() => format(date, 'MMMM d', { locale: enUS }), [date]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   api.get('appointments');
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>31 de algumacoisa</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
