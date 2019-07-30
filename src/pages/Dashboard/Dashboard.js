ae3031efc9571d6fcc0a836ab76120f4e5630f89
Luke Morales <lukemorales@live.com>
2019-07-30 01:15:37 -0300

    feat: Display Appointments

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,16 +1,45 @@
-import React, { useState, useMemo } from 'react';
-import { format, subDays, addDays } from 'date-fns';
+import React, { useState, useMemo, useEffect } from 'react';
+import { format, subDays, addDays, setHours, setMinutes, setSeconds, isBefore, parseISO } from 'date-fns';
 import { enUS } from 'date-fns/locale';
+import { utcToZonedTime } from 'date-fns-tz';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
+import { isEqual } from 'date-fns/esm';
 import api from '@/services/api';
 
 import { Container, Time } from './Dashboard_Styles';
 
+const range = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+
 export default function Dashboard() {
   const [date, setDate] = useState(new Date());
+  const [schedule, setSchedule] = useState([]);
 
   const dateFormatted = useMemo(() => format(date, 'MMMM d', { locale: enUS }), [date]);
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule', {
+        params: { date },
+      });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}h00`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(a => isEqual(parseISO(a.date), compareDate)),
+        };
+      });
+
+      setSchedule(data);
+    }
+    loadSchedule();
+  }, [date]);
 
   function handlePrevDay() {
     setDate(subDays(date, 1));
@@ -20,7 +49,6 @@ export default function Dashboard() {
     setDate(addDays(date, 1));
   }
 
-  api.get('appointments');
   return (
     <Container>
       <header>
@@ -34,22 +62,12 @@ export default function Dashboard() {
       </header>
 
       <ul>
-        <Time past>
-          <strong>08h</strong>
-          <span>Luke Morales</span>
-        </Time>
-        <Time available>
-          <strong>09h</strong>
-          <span>Available</span>
-        </Time>
-        <Time>
-          <strong>10h</strong>
-          <span>Luke Morales</span>
-        </Time>
-        <Time>
-          <strong>11h</strong>
-          <span>Luke Morales</span>
-        </Time>
+        {schedule.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>{time.appointment ? time.appointment.user.name : 'Available'}</span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
