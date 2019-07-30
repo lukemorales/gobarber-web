989b5ded44eb2c0279f3f8ca913d5baeb2233a36
Luke Morales <lukemorales@live.com>
2019-07-30 00:55:07 -0300

    interface: Dashboard Styles

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
 import api from '@/services/api';
-// import {} from './Dashboard_Styles';
+
+import { Container, Time } from './Dashboard_Styles';
 
 export default function Dashboard() {
   api.get('appointments');
   return (
-    <>
-      <h1>Dashboard Component</h1>
-    </>
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de algumacoisa</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08h</strong>
+          <span>Luke Morales</span>
+        </Time>
+        <Time available>
+          <strong>09h</strong>
+          <span>Available</span>
+        </Time>
+        <Time>
+          <strong>10h</strong>
+          <span>Luke Morales</span>
+        </Time>
+        <Time>
+          <strong>11h</strong>
+          <span>Luke Morales</span>
+        </Time>
+      </ul>
+    </Container>
   );
 }
--- src/pages/Dashboard/Dashboard_Styles.js
+++ src/pages/Dashboard/Dashboard_Styles.js
@@ -1,6 +1,53 @@
 import styled from 'styled-components';
 
-export const $ = styled.`
-	
+export const Container = styled.main`
+  max-width: 600px;
+  margin: 50px auto;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-self: center;
+    align-items: center;
+
+    button {
+      background: none;
+    }
+
+    strong {
+      color: #fff;
+      font-size: 24px;
+      margin: 0 15px;
+    }
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+    margin-top: 30px;
+  }
 `;
 
+export const Time = styled.li`
+  padding: 20px;
+  border-radius: 4px;
+  background: #fff;
+
+  opacity: ${({ past }) => (past ? 0.6 : 1)};
+
+  strong {
+    display: block;
+    color: ${({ available }) => (available ? '#999' : '#7159c1')};
+    font-weight: normal;
+    font-size: 20px;
+  }
+
+  span {
+    display: block;
+    margin-top: 3px;
+    color: ${({ available }) => (available ? '#999' : '#666')};
+  }
+`;
